3639811e87e3c442d7b149aed41016e71d037eb5
dvannaken <dvannaken@ucmerced.edu>
2018-04-06 16:20:22 -0700

    fixed commit

---
+++ src/getLogicalCamera.cpp
@@ -0,0 +1,37 @@
+#include <ros/ros.h>
+#include <logical_camera_plugin/logicalImage.h>
+
+
+void lcMessageReceived(const logical_camera_plugin::logicalImage &msg) {
+/*
+	msg.modelName;
+	msg.pose_pos_x;
+	msg.pose_pos_y;
+	msg.pose_pos_z;
+	msg.pose_rot_x;
+	msg.pose_rot_y;
+	msg.pose_rot_z;
+	msg.pose_rot_w;
+*/
+
+ROS_INFO_STREAM("modelName: " << msg.modelName);
+ROS_INFO_STREAM("pose_pos_x: " << msg.pose_pos_x);
+ROS_INFO_STREAM("pose_pos_y: " << msg.pose_pos_y);
+ROS_INFO_STREAM("pose_pos_z: " << msg.pose_pos_z);
+ROS_INFO_STREAM("pose_rot_x: " << msg.pose_rot_x);
+ROS_INFO_STREAM("pose_rot_y: " << msg.pose_rot_y);
+ROS_INFO_STREAM("pose_rot_z: " << msg.pose_rot_z);
+ROS_INFO_STREAM("pose_rot_w: " << msg.pose_rot_w);
+
+}
+
+int main(int argc,char **argv) 
+{
+	ros::init(argc,argv,"getLogicalCamera");
+	ros::NodeHandle nh;
+
+	ros::Subscriber subPose = nh.subscribe("/objectDetected",1000, &lcMessageReceived);
+
+	ros::spin();
+	return 0;
+}

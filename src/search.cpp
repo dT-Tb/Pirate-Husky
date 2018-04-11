25027f59fcfa3e7694005137aad477b66fc65f28
Troy Trinkle <ttrinkle@ucmerced.edu>
2018-04-10 17:35:21 -0700

    Added a prototype skeleton just for showing the general idea of how to control robot behavior

--- src/search.cpp
+++ src/search.cpp
@@ -1,7 +1,22 @@
 #include <ros/ros.h>
 
+bool searching = 1;
+
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "search");
     ros::NodeHandle nh;
+
+    // This is the first possible idea of trying
+    //  to create a way of determining the actions
+    //  of the robot
+    //
+    // Possible improvements: 
+    //  Have a state-machine to control robot behavior
+    //  "Higher Level" node that will pass arguments to 
+    //      the state machine. Could lose efficiency
+    while(searching)
+    {
+        // do something to search the map
+    }
 }

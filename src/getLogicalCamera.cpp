76ee05618fc4757fb13633952a9e51ef89a89135
Troy Trinkle <ttrinkle@ucmerced.edu>
2018-04-24 15:26:19 -0700

    fixed subscriber to actually subscribe to the right topic

--- src/getLogicalCamera.cpp
+++ src/getLogicalCamera.cpp
@@ -29,7 +29,7 @@ int main(int argc,char **argv)
 	ros::init(argc,argv,"getLogicalCamera");
 	ros::NodeHandle nh;
 
-	ros::Subscriber subPose = nh.subscribe("/objectDetected",1000, &lcMessageReceived);
+	ros::Subscriber subPose = nh.subscribe("/objectsDetected",1000, &lcMessageReceived);
 
 	ros::spin();
 	return 0;

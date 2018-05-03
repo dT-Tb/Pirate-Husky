510351acc163a4f15716683dd6472ce1ae24959f
Troy Trinkle <ttrinkle@ucmerced.edu>
2018-05-03 12:47:45 -0700

    Added compatability with a separate planner node

--- src/search.cpp
+++ src/search.cpp
@@ -1,12 +1,36 @@
 #include <ros/ros.h>
 #include <sensor_msgs/LaserScan.h>
 #include <std_msgs/Float32.h>
+#include <std_msgs/Bool.h>
 #include <geometry_msgs/Twist.h>
 
-ros::Subscriber laser_sub;
-ros::Publisher pirate_move_pub;
+ros::Subscriber laser_sub;          // Subscribe to laser range finder
+ros::Subscriber goal_status_sub;    // Subscribe to the current goal state
+ros::Publisher pirate_move_pub;     // Publish move commands to the robot
+ros::Publisher obst_det_pub;        // Publish state of obstacle detection
+ros::Timer search_cutoff;           // Timer to cutoff 'search mode'
 
-bool searching = 1;
+bool searching = 0;     // If the robot is in 'search mode' or not
+bool travelling = 1;    // If planner is working towards a goal state
+
+// Tells the robot to stop 'search mode' and initiate planner again
+void cutoffTimerHandler(const ros::TimerEvent&)
+{
+   searching = 0;
+   travelling = 1;
+
+   // probably need to publish to a topic here so that the robot
+   //   knows to initiate the planner again.
+}
+
+// Check whether the robot has reached its goal or not
+void GoalStateReceived(const std_msgs::Bool& msg)
+{
+    // Should only change travelling to 'false' most of the time
+    //  
+    // When goal is reached it will return 'false' as well
+    travelling = msg.data;
+}
 
 // Receives the laser scan data
 // 
@@ -19,9 +43,17 @@ void LaserHandler(const sensor_msgs::LaserScan& msg)
     {
         if(msg.ranges[i] < 1)
         {
+            // If obstacle detected, then we publish to a topic to 
+            //  describe the current state to the planner node
+            std_msgs::Bool obstacle_detected;
+            obstacle_detected.data = 1;
+            obst_det_pub.publish(obstacle_detected);
+            
+            // We are in 'avoidance mode' and not 'search mode'
             searching = 0;
             geometry_msgs::Twist avoid_cmd;
             
+            // Turn CW or CCW depending on where the obstacle was detected
             if(i < msg.ranges.size() / 2){
                 avoid_cmd.angular.z = -0.5;
                 ROS_INFO_STREAM("Avoiding Left");
@@ -35,8 +67,10 @@ void LaserHandler(const sensor_msgs::LaserScan& msg)
             return;
         }
     }
-
-    searching = 1;
+    
+    // If we were previously in 'search mode' then we restore that mode
+    if(!travelling)
+        searching = 1;
 }
 
 int main(int argc, char** argv)
@@ -46,18 +80,24 @@ int main(int argc, char** argv)
     ros::Rate rate(10);
     
     pirate_move_pub = nh.advertise<geometry_msgs::Twist>("/husky_velocity_controller/cmd_vel", 1000);
+    obst_det_pub = nh.advertise<std_msgs::Bool>("/obstacleDetected", 1000);
+    
+    goal_status_sub = nh.subscribe("/goalStatus", 1000, &GoalStateReceived);
     laser_sub = nh.subscribe("/scan", 1000, &LaserHandler);
     
+    search_cutoff = nh.createTimer( ros::Duration(30.0), cutoffTimerHandler );
+
     while(ros::ok())
     {
         ros::spinOnce();
         
         // Currently searching and not avoiding
-        if(searching) 
+        if(searching && !travelling) 
         {
-            // do the thing
+            // In 'search mode' we simply move forward slowly and try
+            //  to detect things with the logical camera
             geometry_msgs::Twist explore;
-            explore.linear.x = 0.75;
+            explore.linear.x = 0.5;
             explore.angular.z = 0;
 
             pirate_move_pub.publish(explore); 

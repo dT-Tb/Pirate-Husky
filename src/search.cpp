9e4558224650e11fb5b1f04914985d8b4c25bf73
Troy Trinkle <ttrinkle@ucmerced.edu>
2018-05-04 12:53:48 -0700

    Added pseudorando magic to the mix

--- src/search.cpp
+++ src/search.cpp
@@ -1,25 +1,16 @@
 #include <ros/ros.h>
 #include <sensor_msgs/LaserScan.h>
-#include <std_msgs/Float32.h>
-#include <std_msgs/Bool.h>
 #include <random_numbers/random_numbers.h>
 #include <geometry_msgs/Twist.h>
-#include <stdlib.h> 
 
 ros::Subscriber laser_sub;          // Subscribe to laser range finder
-ros::Subscriber goal_status_sub;    // Subscribe to the current goal state
 ros::Publisher pirate_move_pub;     // Publish move commands to the robot
-ros::Publisher obst_det_pub;        // Publish state of obstacle detection
-ros::Timer search_cutoff;           // Timer to cutoff 'search mode'
 
-random_numbers::RandomNumberGenerator *RNG;
+random_numbers::RandomNumberGenerator* rng; // rnjesus for randomwalk simulation
 
-bool searching = 1;     // If the robot is in 'search mode' or not
-
-bool obstaceLeft = 0;   // Let robot know if it should move to right
-bool obstaceRight = 0;  // Let robot know if it should move to left 
-// both of these are based on the sensor readings
-int counter = 0;
+bool searching = 1;  // If the robot is in 'search mode' or not
+bool turnLeft = 0;   // Let robot know if it should move to left 
+bool turnRight = 0;  // Let robot know if it should move to right 
 
 // Receives the laser scan data
 // 
@@ -30,10 +21,9 @@ void LaserHandler(const sensor_msgs::LaserScan& msg)
 {
     for(int i = 30; i <= (msg.ranges.size() - 30); i++)
     {
-        if(msg.ranges[i] < 1 && msg.ranges[i] > 0.2)
+        if(msg.ranges[i] < 1 && msg.ranges[i] > 0.3)
         {
-            
-            // We are in 'avoidance mode' and not 'search mode'
+            // We are avoiding obstacles so we stop searching
             searching = 0;
             geometry_msgs::Twist avoid_cmd;
             
@@ -42,28 +32,31 @@ void LaserHandler(const sensor_msgs::LaserScan& msg)
                 avoid_cmd.angular.z = 0.0;
                 avoid_cmd.linear.x = 0.0;
                 pirate_move_pub.publish(avoid_cmd);
-                avoid_cmd.angular.z = -0.5;
-                avoid_cmd.linear.x = 0.0;
                 
-                obstaceRight = 1;
-                obstaceLeft  = 0;
+                avoid_cmd.angular.z = -0.75;
+                turnRight = 0;
+                turnLeft  = 1;
+                
                 ROS_INFO_STREAM("Avoiding Left");
             }
             else{
                 avoid_cmd.linear.x = 0.0;
                 avoid_cmd.angular.z = 0.0;
                 pirate_move_pub.publish(avoid_cmd);
-                avoid_cmd.linear.x = 0.0;
+                
                 avoid_cmd.angular.z = 0.5;
+                turnRight = 1;
+                turnLeft  = 0;
+                
                 ROS_INFO_STREAM("Avoiding Right");
-                obstaceRight = 0;
-                obstaceLeft  = 1;
             }
+            
             pirate_move_pub.publish(avoid_cmd);
             return;
         }
     }
-    // once obstacel has been avoided then we continue with
+    
+    // once obstacle has been avoided then we continue with
     // search protocol
     searching = 1;
 }
@@ -75,46 +68,54 @@ int main(int argc, char** argv)
     ros::Rate rate(10);
     
     pirate_move_pub = nh.advertise<geometry_msgs::Twist>("/husky_velocity_controller/cmd_vel", 1000);
-
     laser_sub = nh.subscribe("/scan", 1000, &LaserHandler);
+    rng = new random_numbers::RandomNumberGenerator();
+    
     while(ros::ok())
     {
         ros::spinOnce();
-        
+        int stop_arc = (int)rng->uniformInteger(1,100);
+
         // Currently searching and not avoiding
         if(searching) 
         {
-            RNG = new random_numbers::RandomNumberGenerator();
             // In 'search mode' we simply move forward slowly and try
             //  to detect things with the logical camera
             geometry_msgs::Twist explore;
         
             // go forward
-            
             explore.linear.x = 0.5;
-            if(obstaceLeft)
-            {
-                ROS_INFO_STREAM("Moving Forward: R");
-                explore.angular.z = -0.2;
-            }
-            if(obstaceRight)
+            explore.angular.z = 0;
+            
+            // In order to get some more movement around the map, we add
+            //  an arc to the robot's movement for a time.
+            // 
+            // A random number to specify when it will stop turning is used,
+            //  and is to make the overall exploration random
+            if(turnLeft)
             {
                 explore.angular.z = 0.2;
+                ROS_INFO_STREAM("Moving Forward: R");
+            }
+            if(turnRight)
+            {
+                explore.angular.z = -0.2;
                 ROS_INFO_STREAM("Moving Forward: L");
             }
-            if( (counter/45) % 33 == 0)
+            if(stop_arc == 31)   // If a 27 is randomly generated
             {
-                explore.angular.z =0.0;
-                obstaceLeft = 0;
-                obstaceRight = 0; 
+                // Stop all arc movements
+                explore.angular.z = 0;
+                turnLeft = 0;
+                turnRight = 0; 
                 ROS_INFO_STREAM("Moving Forward: S");
             }
 
             pirate_move_pub.publish(explore); 
-            
-            counter++; 
         }
         
         rate.sleep();
     }
+
+    delete rng;
 }
